60366d8a6bb8880e08d2b2d2df0b461f887efdcd
Freek Stulp <stulp@clmc.usc.edu>
2012-10-24 11:24:59 +0200

    Functions for arm kinematics

---
+++ tasks/arm/getarmpos.m
@@ -0,0 +1,88 @@
+function [ x ] = getarmpos(angles,link_lengths,ticks,plot_me)
+
+if (nargin==0), [ x ] = testgetarmpos; return; end;
+if (nargin<2), link_lengths=1; end
+if (nargin<3), ticks=[]; end
+if (nargin<4), plot_me=0; end
+
+[ n_dofs n_tt ] = size(angles);
+
+% If link_lengths is a scalar, it is interpreted to be the length of an arm with
+% links of equal length.
+if (isscalar(link_lengths))
+  link_lengths = link_lengths*ones(1,n_dofs)/n_dofs;
+end
+% Check
+if (length(link_lengths) ~= n_dofs)
+  error('number of links (%d) should be equal to number of dofs (%d)',length(link_lengths),n_dofs);
+end
+
+if (isempty(ticks))
+  compute_ticks=1:n_tt;
+else
+  compute_ticks = ticks;
+end
+
+% 2D position of links over time
+x_links = zeros(n_dofs+1,n_tt,2);
+
+for tt=compute_ticks'
+  sum_angles = 0;
+  for dof=1:n_dofs
+    sum_angles = sum_angles + angles(dof,tt);
+    x_links(dof+1,tt,1) = x_links(dof,tt,1) + cos(sum_angles)*link_lengths(dof);
+    x_links(dof+1,tt,2) = x_links(dof,tt,2) + sin(sum_angles)*link_lengths(dof);
+  end
+end
+
+x = squeeze(x_links(end,ticks,:))';
+
+% Plotting
+if (plot_me)
+  % End-effector path
+  plot(squeeze(x_links(end,ticks,1)),squeeze(x_links(end,ticks,2)),'ko-','LineWidth',1)
+  hold on
+  
+  if (plot_me>1)
+    % Bonus plotting
+    
+    % Links
+    plot(squeeze(x_links(:,:,1)),squeeze(x_links(:,:,2)),'-','Color',0.7*ones(1,3))
+    % Joints
+    % remark: don't plot last entry. It is not a joint, but the end-effector
+    plot(squeeze(x_links(1:end-1,:,1)),squeeze(x_links(1:end-1,:,2)),'.','Color',0.3*ones(1,3))
+    
+    % 'Robot body'
+    plot([-0.1 0 0 -0.1 -0.1 0],[0.05 0.05 -0.05 -0.05 0.05 0.05],'-k')
+  end
+  hold off
+  axis equal
+end
+
+
+  function [x] = testgetarmpos
+    % Test this function with some settings
+    
+    % Arm parameters
+    arm_type = 1;
+    n_dofs = 10;
+    arm_length = 1;
+    link_lengths = getlinklengths(arm_type,n_dofs,arm_length);
+    
+    % Angles over time
+    n_tt = 50;
+    angles = repmat(linspace(0,pi/n_dofs,n_tt),n_dofs,[]);
+
+    sp = 1;
+    for plot_me = 1:2 %#ok<FXUP>
+      for every_other_tick = [1 5]
+        subplot(2,2,sp); sp = sp+1;
+        ticks = 1:every_other_tick:n_tt;
+        x = getarmpos(angles,link_lengths,ticks,plot_me);
+      end
+    end
+    
+  end
+
+end
+
---
+++ tasks/arm/getlinklengths.m
@@ -0,0 +1,54 @@
+function link_lengths = getlinklengths(arm_type,n_dofs,arm_length)
+% Return the lenghts of the links of an arm
+%
+% arm_type   - Which type of arm?
+%               1) All links have same length
+%               2) Approximately human link lengths (6-DOF)
+%               3) 'Inverted' human (links the wrong way around)
+%
+% n_dofs     - Number of links (= number of DOFs) 
+%                Default for equidistant links: 10  (armtype==1)
+%                Default for human arm: 6
+%
+% arm_length - Length of the arm
+%                Default for equidistant links: 1  (armtype==1)
+%                Default for human arm: 0.8 (measured on my own arm ;-)
+
+
+if (nargin==0)
+  % Return number of possible types
+  link_lengths = 3;
+  return;
+end
+
+if (arm_type==1)
+  % Set defaults if parameters weren't passed
+  if (nargin<2), n_dofs=10; end
+  if (nargin<3), arm_length=1; end
+
+  % All links have same size
+  link_lengths = arm_length*ones(1,n_dofs)/n_dofs;
+
+else  
+  % Set defaults if parameters weren't passed
+  if (nargin<2), n_dofs=6; end
+  if (nargin<3), arm_length=0.8; end
+  
+  % Approximate relative human arm lengths
+  % http://www.paintdrawpaint.com/2011/01/drawing-basics-proportions-of-arm.html
+  rel_link_lengths_human = [1.5 1.25 0.4 0.15 0.15 0.1];
+  
+  % Convert relative link lengths to absolute values
+  link_lengths = arm_length*rel_link_lengths_human/sum(rel_link_lengths_human);
+  
+  % Choose only first 'n_dofs' degrees of freedom
+  link_lengths = link_lengths(1:n_dofs);
+  
+  if (arm_type==3)
+    % This is a very strange human arm
+    link_lengths = link_lengths(1,end:-1:1);
+  end
+
+end
+
+end
