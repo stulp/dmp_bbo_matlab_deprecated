a93d4598daff6ffb523cdc5949d8c7c08a90cc09
Freek Stulp <stulp@clmc.usc.edu>
2012-11-14 14:51:01 +0100

    Simplification

--- proximodistalmaturation/main_maturation.m
+++ proximodistalmaturation/main_maturation.m
@@ -1,5 +1,5 @@
 figure(1)
-sensitivityanalysis;
+viapoints = sensitivityanalysis;
 
 figure(2)
 if (~exist('results_uncertaintyhandling','var'))
@@ -10,18 +10,5 @@ end
 
 if (~exist('learning_history','var'))
   figure(3)
-  learning_history = maturationoptimization;
+  maturationoptimization;
 end
-
-% Plot exploration magnitude curve (largest eigenvalue of covar)
-figure(4)
-n_dofs = length(task.link_lengths);
-for i_dof=1:n_dofs
-  legend_labels{i_dof} = sprintf('%d',i_dof);
-  for hh=1:length(learning_history)
-    exploration_curve(i_dof,hh) = real(max(eig(squeeze(learning_history(hh).covar(i_dof,:,:)))));
-  end  
-end
-plot(exploration_curve');
-legend(legend_labels)
-
