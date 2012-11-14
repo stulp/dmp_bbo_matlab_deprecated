ce206a565ae9725c4594d575c337bab312881e2d
Freek Stulp <stulp@clmc.usc.edu>
2012-11-14 11:45:45 +0100

    Better plotting of uncertainty handling

--- proximodistalmaturation/uncertaintyhandling.m
+++ proximodistalmaturation/uncertaintyhandling.m
@@ -15,6 +15,9 @@ if (~isempty(varargin))
   end
 end
 
+n_dofs = 6;
+arm_length = 1;
+
 n_arm_types = getlinklengths;
 
 if (run_new_experiments)
@@ -30,10 +33,8 @@ if (run_new_experiments)
   % i.e. how often does C or D lie between A-B
 
   for arm_type=1:n_arm_types
-    n_dofs = 6;
-    link_lengths = getlinklengths(arm_type,n_dofs);
-    %n_dofs = length(link_lengths);
-    results{arm_type} = zeros(n_dofs,n_dofs);
+    link_lengths = getlinklengths(arm_type,n_dofs,arm_length);
+    results{arm_type} = nan*ones(n_dofs,n_dofs);
 
 
     plot_me=0;
@@ -43,6 +44,8 @@ if (run_new_experiments)
     for which_angle_1=1:(n_dofs-1)
       for which_angle_2=(which_angle_1+1):n_dofs
         fprintf('%d %d ',which_angle_1,which_angle_2);
+
+        results{arm_type}(which_angle_1,which_angle_2) = 0;
 
         for experiments=1:n_experiments
           if (mod(experiments,10)==1)
@@ -92,26 +95,37 @@ end
 
 clf
 for arm_type=1:n_arm_types
-  link_lengths = getlinklengths(arm_type);
-  n_dofs = length(link_lengths);
+  link_lengths = getlinklengths(arm_type,n_dofs,arm_length);
 
   subplot(2,n_arm_types,arm_type)
   getarmpos(0.0*ones(1,n_dofs),link_lengths,1,2);
   xlim([-0.1 1.1])
 
   subplot(2,n_arm_types,n_arm_types+arm_type)
-  bar3(results{arm_type});
+  h = bar3(results{arm_type});
+  %h = bar3(results{arm_type}(1:n_dofs-1,2:n_dofs));
+  remove_empty_bars(h);
+  axis equal
   axis square;
-  axis tight;
-  xlabel('proximal joint');
-  ylabel('distal joint');
+  xlabel('distal joint');
+  ylabel('proximal joint');
   zlabel('% that distal joint doesnt matter')
-  %set(gca,'XTick',1:n_dofs)
-  %set(gca,'XTickLabel',n_dofs:-1:1)
-  %set(gca,'YTick',1:n_dofs)
-  %set(gca,'YTickLabel',n_dofs:-1:1)
+  set(gca,'YTick',1:n_dofs-1)
+  set(gca,'XTick',2:n_dofs)
   view(-45,20);
+  zlim([0 1])
   drawnow
 end
+
+
+  function remove_empty_bars(hBars)
+    % Taken from: http://stackoverflow.com/questions/2050367/how-to-hide-zero-values-in-bar3-plot-in-matlab
+    for iSeries = 1:numel(hBars)
+      zData = get(hBars(iSeries),'ZData');  %# Get the z data
+      index = logical(kron(zData(2:6:end,2) == 0,ones(6,1)));  %# Find empty bars
+      zData(index,:) = nan;                 %# Set the z data for empty bars to nan
+      set(hBars(iSeries),'ZData',zData);    %# Update the graphics objects
+    end
+  end
 
 end
