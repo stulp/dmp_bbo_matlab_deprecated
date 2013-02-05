91118c05789b6d74e9cbefcb1c3139964645f8c5
Freek Stulp <stulp@clmc.usc.edu>
2013-02-05 11:36:14 +0100

    Adapted dmp_bbo_example to new input parameters

--- dmp_bbo_example.m
+++ dmp_bbo_example.m
@@ -4,7 +4,7 @@ addpath(genpath('tasks/'))
 
 % Get the task to be optimized
 
-% A very simple 1-D DMP viapoint task
+% A very simple 2-D DMP viapoint task
 task = task_viapoint;
 % The JMLR style n-DOF kinmatically simulated arm task
 %n_dofs = 2;
@@ -14,24 +14,20 @@ task = task_viapoint;
 % Initial covariance matrix for exploration 
 covar_init = 5*eye(size(task.theta_init,2));
 
-% Number of updates, roll-outs per update, weighting method, and covariance
-% update method
-n_updates = 100;
-K = 15;
-weighting_method = 2; % 1=CEM, 2=CMAES, 3=PI2
-eliteness = ceil(K/2);
-%weighting_method = 3; eliteness = 10; % PI2 style weighting
-covar_update = 0.99; %#ok<NASGU> % Set 0 < covar_update < 1 to decay exploration over time
-covar_bounds = []; % No bounds
-%covar_update = 2;    % Reward-weighted averaging update of covar
-if (covar_update>=1)
-  % For covariance matrix updating with RWA, a lower bound on the
-  % eigenvalues is recommended to avoid premature convergence
-  covar_bounds = [0.1]; %#ok<NBRAK> % 0.05 10]; %#ok<NBRAK> % Lower/upper bounds on covariance matrix
-end
+% Number of updates, roll-outs per update
+n_updates =  25;
+n_samples =  15;
+
+% Weighting method, and covariance update method
+update_parameters.weighting_method    = 'PI-BB'; % {'PI-BB','CMA-ES'}
+update_parameters.eliteness           =      10;
+update_parameters.covar_update        = 'PI-BB'; % {'PI-BB','CMA-ES'}
+update_parameters.covar_full          =       0; % 0 -> diag, 1 -> full
+update_parameters.covar_learning_rate =     0.8; % No lowpass filter
+update_parameters.covar_bounds        =   [0.1 0.01]; %#ok<NBRAK> 
 
 clf
-evolutionaryoptimization(task,task.theta_init,covar_init,n_updates,K,eliteness,weighting_method,covar_update)
+evolutionaryoptimization(task,task.theta_init,covar_init,n_updates,n_samples,update_parameters)
 
 
 
--- tasks/task_viapoint.m
+++ tasks/task_viapoint.m
@@ -5,7 +5,7 @@ if (nargin<3), viapoint  = [0.4 0.7]; end
 if (nargin<4), viapoint_time_ratio = 0.5; end
 
 task.name = 'viapoint';
-task.perform_rollout = @perform_rollout_viapoint;
+task.perform_rollouts = @perform_rollouts_viapoint;
 
 % Initial and goal state
 task.y0 = y0;
@@ -21,39 +21,54 @@ task.viapoint_time_step = round(viapoint_time_ratio*task.time/task.dt);
 
 task.order=2; % Order of the dynamic movement primitive
 % Next values optimized for minimizing acceleration in separate learning session
-task.theta_init = [37.0458   -4.2715   27.0579   13.6385; 37.0458   -4.2715   27.0579   13.6385];
+%task.theta_init = [37.0458   -4.2715   27.0579   13.6385; 37.0458   -4.2715   27.0579   13.6385];
+task.theta_init = zeros(2,2);
 
 
 addpath dynamicmovementprimitive/
 
 % Now comes the function that does the roll-out and visualization thereof
-  function cost = perform_rollout_viapoint(task,theta,plot_me,color)
+  function costs = perform_rollouts_viapoint(task,thetas,plot_me,color)
     
-    trajectory = dmpintegrate(task.y0,task.g,theta,task.time,task.dt,task.time_exec);
-
-    % Cost due to distance from viapoint
-    ys = trajectory.y;
-    dist_to_viapoint = sqrt(sum((ys(task.viapoint_time_step,:)-viapoint).^2));
-    cost(2) = dist_to_viapoint;
+    K = size(thetas,2);
     
-    % Cost due to acceleration
-    ydds = trajectory.ydd;
-    sum_ydd = sum((sum(ydds.^2,2)));
-    cost(3) = sum_ydd/100000;
+    for k=K:-1:1
+      theta = squeeze(thetas(:,k,:));
+    
+      trajectory = dmpintegrate(task.y0,task.g,theta,task.time,task.dt,task.time_exec);
 
-    % Total cost is the sum of all the subcomponent costs 
-    cost(1) = sum(cost(2:end));
+      % Cost due to distance from viapoint
+      ys = trajectory.y;
+      dist_to_viapoint = sqrt(sum((ys(task.viapoint_time_step,:)-viapoint).^2));
+      costs(k,2) = dist_to_viapoint;
+    
+      % Cost due to acceleration
+      ydds = trajectory.ydd;
+      sum_ydd = sum((sum(ydds.^2,2)));
+      costs(k,3) = sum_ydd/100000;
+
+      % Total cost is the sum of all the subcomponent costs
+      costs(k,1) = sum(costs(k,2:end));
+      
+      if (plot_me)
+        linewidth = 1;
+        if (nargin<4)
+          color = 0.8*ones(1,3);
+        end
+        if (k==1)
+          color=0.5*color;
+          linewidth = 3;
+        end
+        plot(ys(:,1),ys(:,2),'Color',color,'LineWidth',linewidth)
+        plot([task.viapoint(1) ys(task.viapoint_time_step,1)],[task.viapoint(2) ys(task.viapoint_time_step,2)],'Color',color)
+      end
+
+    end
 
     if (plot_me)
-      if (nargin<4)
-        color = 0.8*ones(1,3);
-      end
-      plot(task.viapoint(1),task.viapoint(2),'*g')
-      plot(ys(:,1),ys(:,2),'Color',color)
-      plot([task.viapoint(1) ys(task.viapoint_time_step,1)],[task.viapoint(2) ys(task.viapoint_time_step,2)],'Color',0.5*color)
+      plot(task.viapoint(1),task.viapoint(2),'og')
       axis([-0.1 1.1 -0.1 1.1])
-    end
-      
+    end      
 
   end
 
