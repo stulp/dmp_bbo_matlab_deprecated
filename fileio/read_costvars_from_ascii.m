dd106f2b372ee5a96ef0fcba94a493dd20c0644d
Freek Stulp <stulp@clmc.usc.edu>
2013-02-06 12:20:00 +0100

    Added matlab and cpp files for evaluating costs in external programs

---
+++ fileio/read_costvars_from_ascii.m
@@ -0,0 +1,18 @@
+function cost_vars = read_costvars_from_ascii(directory,current_update)
+    
+if (nargin<2)
+  filename = sprintf('%s/current_update.txt',directory);
+  current_update = load(filename);
+end
+
+output_directory = sprintf('%s/%03d_txt_files',directory,current_update);
+
+filename = sprintf('%s/number_of_trials.txt',output_directory);
+n_samples = load(filename);
+
+for i_sample=1:n_samples
+  filename = sprintf('%s/%02d_costvars.txt',output_directory,i_sample);
+  cost_vars(i_sample,:,:) = load(filename);
+end
+
+end
