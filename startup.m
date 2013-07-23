349245962ce6e4cfaeae5b80f365eb198d00299f
Freek Stulp <stulp@clmc.usc.edu>
2013-07-23 07:50:51 +0200

    Added paths

--- startup.m
+++ startup.m
@@ -3,3 +3,10 @@ addpath(genpath('dynamicmovementprimitive'))
 addpath(genpath('evolutionaryoptimization'))
 addpath(genpath('fileio'))
 addpath(genpath('tasks'))
+if (exist('dealiasing','dir'))
+  addpath(genpath('dealiasing'))
+end
+if (exist('proximodistalmaturation','dir'))
+  addpath(genpath('proximodistalmaturation'))
+end
+  
