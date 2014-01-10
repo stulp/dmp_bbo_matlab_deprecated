5cf983967de2ec4c434c860edcaab95653671ad5
Freek Stulp <freek.stulp@ensta-paristech.fr>
2014-01-10 15:51:19 +0100

    Quadratic cost

--- tasks/petanque/task_petanque.m
+++ tasks/petanque/task_petanque.m
@@ -40,7 +40,7 @@ addpath dynamicmovementprimitive/
       ball_landed =  cost_vars(end,4:6);
       
       dist = sqrt(sum((ball_landed-ball_goal).^2));
-      cost = (100*dist);
+      cost = dist*dist;
 
       costs(k,:) = cost;
     end
